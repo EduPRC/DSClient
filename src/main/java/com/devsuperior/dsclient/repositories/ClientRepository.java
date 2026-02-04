ad4ac9cfcdf24f0d15843fef8b6bd9e8dbb7169f
Eduardo Canos <eduardo.p.r.canos@gmail.com>
2026-02-04 17:21:08 -0300

    feat: add ClientDTO and test environment configuration

--- src/main/java/com/devsuperior/dsclient/repositories/ClientRepository.java
+++ src/main/java/com/devsuperior/dsclient/repositories/ClientRepository.java
@@ -4,5 +4,5 @@ import com.devsuperior.dsclient.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    
+
 }
